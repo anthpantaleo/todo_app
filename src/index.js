3da95cf95d67770871ed69bcccf16346d352ac75
anthpantaleo <anthpantaleo@gmail.com>
2022-10-17 21:34:35 -0400

    initial structure

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./styles.css";
+import { todo } from "./todo";
+
+console.log("Anthony");
+
+todo();
---
+++ src/todo.js
@@ -0,0 +1,7 @@
+import { format, compareAsc } from "date-fns";
+
+function todo() {
+  console.log("called from todo.");
+}
+
+export { todo };
