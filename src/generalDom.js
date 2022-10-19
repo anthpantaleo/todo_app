b1dc474ed96d77ffb6e853d075cd60811b806615
anthpantaleo <anthpantaleo@gmail.com>
2022-10-18 20:20:30 -0400

    styling initial

--- src/generalDom.js
+++ src/generalDom.js
@@ -4,9 +4,9 @@ const generalDomControl = (() => {
   themeIcon.onclick = function () {
     document.body.classList.toggle("dark-theme");
     if (document.body.classList.contains("dark-theme")) {
-      themeIcon.src = "../dist/images/sun.png";
+      themeIcon.src = "../dist/images/sunpixel.png";
     } else {
-      themeIcon.src = "../dist/images/moon.png";
+      themeIcon.src = "../dist/images/half-moon.png";
     }
   };
 })();
